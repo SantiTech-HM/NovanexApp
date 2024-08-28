3e09ec96b3498a7f6aa3b8acfcc9aaf4d091e4b5
SantiTech-HM <shoyo01@misena.edu.co>
2024-08-28 07:51:28 -0500

    Proyecto novanex

---
+++ src/main/java/com/novanex/App/NovanexApplication.java
@@ -0,0 +1,13 @@
+package com.novanex.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovanexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovanexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novanex/App/NovanexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novanex.App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovanexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
